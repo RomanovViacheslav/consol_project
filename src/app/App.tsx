f93d1e20e689f0b85875c9b37f51a4f6a230098f
Viacheslav Romanov <kozyr52@gmail.com>
2023-06-27 02:51:53 +0300

    add routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,8 @@ import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "./providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter, useTheme } from "./providers";
+
 
 export function App() {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +15,7 @@ export function App() {
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 }
