dd215d4439cf7cc8c10cccf80ceb58dd53180951
Viacheslav Romanov <kozyr52@gmail.com>
2023-08-14 01:05:26 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
-import React, { Suspense, useContext, useState } from "react";
-import "./styles/index.scss";
-import { Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { classNames } from "shared/lib/classNames/classNames";
-import { AppRouter, useTheme } from "./providers";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-import { useTranslation } from "react-i18next";
-
+import React, { Suspense, useContext, useState } from 'react';
+import './styles/index.scss';
+import { Route, Routes, Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
+import { AppRouter, useTheme } from './providers';
 
 export function App() {
   const { theme } = useTheme();
   return (
-    <div className={classNames("app", {}, [theme])}>
+    <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <div className="content-page">
           <Sidebar />
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function MainPage() {
   const { t } = useTranslation();
-  return <div>
-    {t("Главная страница")}
-    {t('dddddd')}
-    </div>;
+  return (
+    <div>
+      {t('Главная страница')}
+      {t('dddddd')}
+    </div>
+  );
 }
